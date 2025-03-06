026103adb1e0e235ff2e5396639ccebd74d735ac
Andrew Davis <dynamicdebug@gmail.com>
2025-03-06 16:33:32 -0500

    minor date formatting comment

--- src/lib/datetime.ts
+++ src/lib/datetime.ts
@@ -3,7 +3,7 @@ export const SHORT_MONTH_DATE_FORMATTER = new Intl.DateTimeFormat('en-US', {
   month: 'short',
   day: 'numeric',
 });
-
+// date format: 1st January 2025
 export const LONG_MONTH_DATE_FORMATTER = new Intl.DateTimeFormat('en-US', {
   year: 'numeric',
   month: 'long',
